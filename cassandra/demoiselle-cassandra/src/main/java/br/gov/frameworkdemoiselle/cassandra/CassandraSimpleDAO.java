26f6d937080d56b18a8d0974add4b93e6f94d1ca
rodrigo.hjort@gmail.com <rodrigo.hjort@gmail.com@2453fe31-b880-c01f-5feb-1bedba4748fc>
2010-11-10 20:46:29 +0000

    Primeira inclusão dos fontes.

---
+++ cassandra/demoiselle-cassandra/src/main/java/br/gov/frameworkdemoiselle/cassandra/CassandraSimpleDAO.java
@@ -0,0 +1,33 @@
+package br.gov.frameworkdemoiselle.cassandra;
+
+import java.util.List;
+
+public abstract class CassandraSimpleDAO<T> {
+
+	public void insert(final T object) {
+	}
+
+	public void delete(final T object) {
+	}
+
+	public void delete(final String key) {
+	}
+
+	public T get(final String key) {
+		return null;
+	}
+
+	public List<T> get(final Iterable<String> keys) {
+		return null;
+	}
+
+	public List<T> getRange(final String keyStart, final String keyEnd,
+			final int amount) {
+		return null;
+	}
+
+	public List<T> get(final String key, final Iterable<String> columns) {
+		return null;
+	}
+	
+}
---
+++ cassandra/demoiselle-cassandra/src/main/java/br/gov/frameworkdemoiselle/cassandra/annotation/CassandraColumn.java
@@ -0,0 +1,24 @@
+package br.gov.frameworkdemoiselle.cassandra.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+//import org.apache.cassandra.thrift.ConsistencyLevel;
+
+//import org.apache.cassandra.thrift.ConsistencyLevel;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface CassandraColumn {
+
+	String keyspace();
+
+	String columnFamily();
+
+	String secondaryColumnFamily() default "";
+
+//	ConsistencyLevel consistency() default ConsistencyLevel.QUORUM;
+
+}
