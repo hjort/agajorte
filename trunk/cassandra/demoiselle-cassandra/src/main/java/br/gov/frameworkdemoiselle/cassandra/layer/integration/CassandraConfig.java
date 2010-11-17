f6bd798f354f8d053ef1fa096fa61cef3451fed6
rodrigo.hjort@gmail.com <rodrigo.hjort@gmail.com@2453fe31-b880-c01f-5feb-1bedba4748fc>
2010-11-17 13:27:49 +0000

    Alteração das configurações.

--- trunk/cassandra/demoiselle-cassandra/src/main/java/br/gov/frameworkdemoiselle/cassandra/layer/integration/CassandraConfig.java
+++ trunk/cassandra/demoiselle-cassandra/src/main/java/br/gov/frameworkdemoiselle/cassandra/layer/integration/CassandraConfig.java
@@ -14,19 +14,19 @@ public class CassandraConfig implements IConfig {
 
 	private static CassandraConfig instance;
 	
-	@ConfigKey(name = "component.demoiselle.cassandra.server_nodes",
+	@ConfigKey(name = "demoiselle.cassandra.server_nodes",
 			type = ConfigType.PROPERTIES, resourceName = Constant.FRAMEWORK_CONFIGURATOR_FILE)
 	private String[] serverNodes;
 
-	@ConfigKey(name = "component.demoiselle.cassandra.default_keyspace",
+	@ConfigKey(name = "demoiselle.cassandra.default_keyspace",
 			type = ConfigType.PROPERTIES, resourceName = Constant.FRAMEWORK_CONFIGURATOR_FILE)
 	private String defaultKeyspace;
 
-	@ConfigKey(name = "component.demoiselle.cassandra.default_consistency",
+	@ConfigKey(name = "demoiselle.cassandra.default_consistency",
 			type = ConfigType.PROPERTIES, resourceName = Constant.FRAMEWORK_CONFIGURATOR_FILE)
 	private String defaultConsistency = ConsistencyLevel.QUORUM.name();
 
-	@ConfigKey(name = "component.demoiselle.cassandra.serialize_unknown",
+	@ConfigKey(name = "demoiselle.cassandra.serialize_unknown",
 			type = ConfigType.PROPERTIES, resourceName = Constant.FRAMEWORK_CONFIGURATOR_FILE)
 	private boolean serializeUnknown;
 	
