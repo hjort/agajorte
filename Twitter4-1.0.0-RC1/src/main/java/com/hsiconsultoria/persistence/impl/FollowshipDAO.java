0ec495519de18d3a63dab9d759b497eb373e4e13
rodrigo.hjort@gmail.com <rodrigo.hjort@gmail.com@2453fe31-b880-c01f-5feb-1bedba4748fc>
2010-11-17 15:04:44 +0000

    Criação de tag 1.0.0-RC1.

---
+++ Twitter4-1.0.0-RC1/src/main/java/com/hsiconsultoria/persistence/impl/FollowshipDAO.java
@@ -0,0 +1,36 @@
+package com.hsiconsultoria.persistence.impl;
+
+import java.util.List;
+
+import br.gov.frameworkdemoiselle.cassandra.CassandraColumnDAO;
+
+import com.hsiconsultoria.bean.Followship;
+import com.hsiconsultoria.persistence.IFollowshipDAO;
+
+/**
+ * @author Rodrigo Hjort
+ */
+@SuppressWarnings("serial")
+public class FollowshipDAO extends CassandraColumnDAO<Followship> implements IFollowshipDAO {
+
+	public List<String> findFollowingsLogins(String follower) {
+		
+		List<String> logins = getColumns(follower);
+		
+		if (logins == null || logins.isEmpty())
+			return null;
+		
+		return logins;
+	}
+
+	public List<String> findFollowersLogins(String followed) {
+
+		List<String> logins = getColumnsBySecondary(followed);
+		
+		if (logins == null || logins.isEmpty())
+			return null;
+		
+		return logins;
+	}
+
+}
---
+++ Twitter4-1.0.0-RC1/src/main/java/com/hsiconsultoria/persistence/impl/UserDAO.java
@@ -0,0 +1,24 @@
+package com.hsiconsultoria.persistence.impl;
+
+import java.util.List;
+
+import br.gov.frameworkdemoiselle.cassandra.CassandraEntityDAO;
+
+import com.hsiconsultoria.bean.User;
+import com.hsiconsultoria.persistence.IUserDAO;
+
+/**
+ * @author Rodrigo Hjort
+ */
+@SuppressWarnings("serial")
+public class UserDAO extends CassandraEntityDAO<User> implements IUserDAO {
+	
+	public User findByLogin(String login) {
+		return get(login);
+	}
+
+	public List<User> findByLogins(Iterable<String> logins) {
+		return get(logins);
+	}
+
+}
